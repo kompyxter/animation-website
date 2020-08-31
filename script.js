e1e5d8aa576dc6f79331ddd892ee93bfe6de9baa
kompyxter <68939915+kompyxter@users.noreply.github.com>
2020-08-31 16:13:26 +0300

    1

---
+++ script.js
@@ -0,0 +1,8 @@
+$(function(){
+	$('#animation1').animate({'width': '180', 'height': '760', 'padding': '15'}, 2000)
+	$('#animation2').animate({'width': '305', 'height': '155', 'padding-top': '45'}, 1500)
+	$('#animation3').animate({'width': '150', 'height': '210', 'padding-top': '60'}, 1400)
+	$('#animation4').animate({'width': '150', 'height': '210', 'padding-top': '60'}, 2500)
+	$('#animation5').animate({'width': '300', 'height': '388', 'padding-top': '60'}, 2000)
+	$('#animation6').animate({'width': '800', 'height': '200'}, 2011)
+});	
